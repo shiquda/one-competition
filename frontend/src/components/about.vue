aa976284bb18238d78c863250a126043dde281bf
shiquda <czy1847259360@gmail.com>
2024-10-19 23:00:31 +0800

    init: 千里之行，始于这里。

---
+++ frontend/src/components/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>关于该网站</h1>
+        <p>One 竞赛是一个<strong>开放的竞赛信息聚合平台</strong>，我们的愿景是让竞赛信息更加透明，让竞赛资源更加公平。</p>
+    </div>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
+
+
