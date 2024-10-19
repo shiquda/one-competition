0f949cbe7ffbe8ac887f372905a27070284aae42
shiquda <czy1847259360@gmail.com>
2024-10-20 00:27:00 +0800

    chore: 添加依赖

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    vueIndentScriptAndStyle: true,
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf',
+};
