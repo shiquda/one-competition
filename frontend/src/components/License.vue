1f8f6914d353c136ac28adf0083a24531f880b52
shiquda <czy1847259360@gmail.com>
2024-12-18 08:38:54 +0800

    feat: add github icon

--- frontend/src/components/License.vue
+++ frontend/src/components/License.vue
@@ -1,3 +1,12 @@
 <template>
-  <p>One 竞赛 © 2024</p>
+  <div class="flex justify-center items-center">
+    <p>One 竞赛 © 2024</p>
+    <a
+      href="https://github.com/shiquda/one-competition"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-gray-700 hover:text-blue-500 ml-4"
+      ><i class="fa-brands fa-github fa-lg"></i
+    ></a>
+  </div>
 </template>
